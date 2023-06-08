e9532990b9e8c44115bc1f233f60534ee2390ab8
Subin Lee <ewaterbinn@gmail.com>
2023-06-08 15:58:47 +0900

    제너레이터와 이터레이터 필기

--- 01-iteratable.js
+++ 01-iteratable.js
@@ -87,3 +87,130 @@ console.clear();
 const a = [1, 2];
 console.log([...a, ...[3, 4]]); //[1,2,3,4]
 console.log([...a, ...arr, ...set, ...map.values()]); //[1, 2, 1, 2, 3, 1, 2, 3, 1, 2, 3]
+
+/* # 제너레이터와 이터레이터 
+- 제너레이터 : 이터레이터이자 이터러블을 생성하는 함수 
+*/
+
+function* gen() {
+  yield 1;
+  yield 2;
+  yield 3;
+  return 100; // return 값도 생성 가능 - 순회할 때 return 값은 순회하지 않음
+}
+let iter = gen();
+console.log(iter[Symbol.iterator]() == iter);
+console.log(iter.next());
+
+/* 어떠한 값이든 iterator 이면 순회할 수 있는 형태로 만들 수 있다. 
+제너레이터가 그렇게 만들 수 있는 애 */
+
+for (const a of gen()) console.log(a); //이렇게 순회도 가능
+
+/* -odds 출력 실습
+l 은 limit을 뜻함
+*/
+
+function* odds(l) {
+  for (let i = 0; i < l; i++) {
+    if (i % 2) yield i;
+  }
+}
+
+let iter3 = odds(10);
+console.log(iter3.next());
+console.log(iter3.next());
+console.log(iter3.next());
+console.log(iter3.next());
+console.log(iter3.next());
+console.log(iter3.next());
+/**
+ * { value: 1, done: false }
+{ value: 3, done: false }
+{ value: 5, done: false }
+{ value: 7, done: false }
+{ value: 9, done: false }
+{ value: undefined, done: true }
+ */
+
+function* infinity(i = 0) {
+  while (true) yield i++;
+}
+
+function* limit(l, iter) {
+  for (const a of iter) {
+    yield a;
+    if (a == l) return;
+  }
+}
+
+function* odds2(l) {
+  for (const a of infinity(1)) {
+    if (a % 2) yield a;
+    if (a == l) return;
+  }
+}
+
+function* odds3(l) {
+  for (const a of limit(l, infinity(1))) {
+    if (a % 2) yield a;
+  }
+}
+
+let iter4 = odds3(10);
+
+console.log(iter4.next());
+console.log(iter4.next());
+console.log(iter4.next());
+console.log(iter4.next());
+console.log(iter4.next());
+console.log(iter4.next());
+/**
+ * 
+{ value: 1, done: false }
+{ value: 3, done: false }
+{ value: 5, done: false }
+{ value: 7, done: false }
+{ value: 9, done: false }
+{ value: undefined, done: true }
+ */
+
+for (const a of odds3(40)) console.log(a);
+/**
+ * 1
+3
+5
+7
+9
+11
+13
+15
+17
+19
+21
+23
+25
+27
+29
+31
+33
+35
+37
+39
+ */
+
+/**
+ * # for of, 전개 연산자, 구조 분해, 나머지 연산자
+ */
+
+console.log(...odds(10)); //13579
+console.log(...odds(10), ...odds(20)); //1 3 5 7 9 1 3 5 7 9 11 13 15 17 19
+
+const [head, ...tail] = odds(5);
+console.log(head); //1
+console.log(tail); //[3]
+
+const [aa, bb, ...rest] = odds(10);
+console.log(aa); //1
+console.log(bb); //3
+console.log(rest); //[5,7,9]
